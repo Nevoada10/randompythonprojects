8655c717ef20e3e10e7e36dce6a435517e77f614
Uriel Neves Silva <144985253+Nevoada10@users.noreply.github.com>
2024-10-19 16:41:39 +0200

    .

---
+++ Calculations/velocidademedia.py
@@ -0,0 +1,19 @@
+# Defining the known values
+distance = 1500 # meters
+time = 60*3 + 26 # seconds
+
+# Calculating the average speed in m/s
+average_speed_m_s = distance / time
+
+# Converting to km/h (1 m/s = 3.6 km/h)
+average_speed_km_h = average_speed_m_s * 3.6
+
+# Rounding to two decimal places
+average_speed_m_s = round(average_speed_m_s, 2)
+average_speed_km_h = round(average_speed_km_h, 2)
+
+
+# Debugging
+print(f"Distance: {distance} meters | Time: {time} seconds")
+# Printing the results
+print(f"Average speed: {average_speed_m_s} m/s or {average_speed_km_h} km/h")
